d3a3465d3d631425a7cb1d0e215f859b411fd447
David Carboni <david@carboni.io>
2019-02-05 21:54:47 +0000

    Tuning to taste.

--- build.sh
+++ build.sh
@@ -5,15 +5,12 @@ base=$PWD
 
 # Update functions
 cd functions && zip -r functions.zip .
-#gsutil -m cp -r functions.zip gs://mamecloud-functions/
+gsutil -m cp -r functions.zip gs://mamecloud-functions/
 rm functions.zip
 cd $base
 
-# The buckets we want to trigger on:
-declare -a buckets=(
-  "mamecloud"
-)
-
 # TODO: deploy from a source repo?
-echo gcloud functions deploy unzip --region=europe-west1 --runtime=python37 --source=gs://mamecloud-functions/functions.zip --memory=128MB --trigger-bucket --trigger-resource=mamecloud --trigger-event=google.storage.object.finalize --entry-point=unzip
+gcloud functions deploy unzip-file --region=europe-west1 --runtime=python37 --source=gs://mamecloud-functions/functions.zip --memory=128MB --trigger-event=google.storage.object.finalize --trigger-resource=mamecloud --entry-point=unzip
 
+gsutil rm gs://mamecloud/cabinets/test.zip
+gsutil cp ~/Downloads/test.zip gs://mamecloud/cabinets/test.zip
